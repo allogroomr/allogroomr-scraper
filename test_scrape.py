99111f51226036eb9f415e00c4cb4b29b18e6552
allogroomr <allogroomr@gmail.com>
2025-08-25 19:55:56 -0500

    Update test_scrape.py
    Modify test_scrape.py to save results into a CSV instead of just printing.

--- test_scrape.py
+++ test_scrape.py
@@ -1,11 +1,14 @@
-import requests
+import csv, requests
 from bs4 import BeautifulSoup
 
 url = "https://quotes.toscrape.com/"
 res = requests.get(url)
 soup = BeautifulSoup(res.text, 'lxml')
 
-for quote in soup.select (".quote"):
-	text = quote.select_one(".text").get_text()
-	author = quote.select_one(".author").get_text()
-	print(f"{text} - {author}")
+with open("quotes.csv", "w", newline="") as f:
+	writer = csv.writer(f)
+	writer.writerow(["quote", "author"])
+	for quote in soup.select (".quote"):
+		text = quote.select_one(".text").get_text()
+		author = quote.select_one(".author").get_text()
+		writer.writerow([text, author])
